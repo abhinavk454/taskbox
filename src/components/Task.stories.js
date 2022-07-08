3def570f89813fe8c9922b0eb73d0ab43cefda16
Abhinav Kumar <32016468+abhinavk454@users.noreply.github.com>
2022-07-08 03:38:28 +0000

    Day01

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -1,2 +1,34 @@
 import React from "react";
 import Task from "./Task";
+
+export default {
+  component: Task,
+  title: "Task", //how to refer to the component in the sidebar of the Storybook app
+};
+
+const Template = (args) => <Task {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  task: {
+    id: "1",
+    title: "Test task",
+    state: "TASK_INBOX",
+  },
+};
+
+export const Pinned = Template.bind({});
+Pinned.args = {
+  task: {
+    ...Default.args,
+    state: "TASK_PINNED",
+  },
+};
+
+export const Archived = Template.bind({});
+Archived.args = {
+  task: {
+    ...Default.args,
+    state: "TASK_ARCHIVED",
+  },
+};
